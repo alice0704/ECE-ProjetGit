584d40713abdfa8f4a31ba0458b37f165bb6d2c8
alice0704 <alice.sabot@edu.ece.fr>
2018-02-13 11:30:30 +0100

    creation main.cpp

---
+++ alice1.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alice1
+//
+//  Created by Alice SABOT on 13/02/2018.
+//  Copyright © 2018 Alice SABOT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
